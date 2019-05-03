99941f549eb71bb25df6eaf6b469e1f079d9fde2
lewmlp <lew98@yandex.ru>
2019-05-03 18:17:04 +0300

    установил нод экспресс, монгодб, подключился к дб и запустил локальный сервер

---
+++ site5/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var mongoClient = require("mongodb").MongoClient;
+var db = new mongoClient("mongodb://localhost:27017/", {useNewUrlParser: true});
+var objectId = require("mongodb").ObjectID;
+var application = express();
+var jsonParser = express.json();
+application.use(express.static(__dirname + "/static"));
+
+
+db.connect(function(err, client){
+    if (err) return console.log(err);
+    application.locals.collection = client.db("lab5").collection("sketch");
+    application.listen(3000, function(){
+        console.log('OK');
+    });
+});
